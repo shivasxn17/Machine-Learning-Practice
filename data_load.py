180765d14f66925c81be8444848564c3b6c06397
rex17 <shivamsaxena17@live.com>
2017-11-26 17:29:02 +0530

    dataset ops and plots added

--- data_load.py
+++ data_load.py
@@ -1,3 +1,6 @@
+import time
+print(time.gmtime())
+start_time = time.clock()
 # Load libraries
 import pandas
 from pandas.tools.plotting import scatter_matrix
@@ -13,6 +16,56 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
+
+
 print("libraries loaded successfully")
 
+# Load dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv("iris.data", names=names)
 
+#print(dataset)
+
+# rows x columns(attributes)
+print(dataset.shape)
+print("=========================================================================")
+
+# view data 
+print(dataset.head(2))
+print("=========================================================================")
+
+# data description min max standard deviation mean
+print(dataset.describe())
+print("=========================================================================")
+
+# class distributions count
+print(dataset.groupby("class").size())
+print("=========================================================================")
+
+# data visualization with univariate plot
+
+# box and whisker plots
+
+# dataset.plot(kind = "box",
+# 			subplots = "True",
+# 			layout = (2,2),
+# 			sharex = "False",
+# 			sharey = "False")
+# plt.show()
+
+# generate the histograms for given data
+# dataset.hist()
+# plt.show()
+
+# data visualization with multivariate plot 
+# scatter plot matrix 
+scatter_matrix(dataset)
+plt.show()
+
+
+
+
+end_time = time.clock()
+
+print("Execution time :", end_time-start_time)
